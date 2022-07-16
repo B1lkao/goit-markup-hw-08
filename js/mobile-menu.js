0c25e63aa3ccf49053bcdeb8f1aacbfd81d1f14a
B1lkao <hrabousolesia@gmail.com>
2022-07-16 17:06:09 +0300

    .

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,14 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', toggleMenu);
-
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    if (mobileMenuRef.classList.contains('is-open')) {
-      toggleMenu();
-    }
-  });
-
+  const refs = {
+    openMenuBtn: document.querySelector('.header__mobile-open-btn'),
+    closeMenuBtn: document.querySelector('.header__mobile-close-btn'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
   function toggleMenu() {
-    menuBtnRef.classList.toggle('is-open');
-    mobileMenuRef.classList.toggle('is-open');
-    document.body.classList.toggle('modal-open');
-
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
